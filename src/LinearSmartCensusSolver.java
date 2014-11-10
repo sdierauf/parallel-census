4a95526be75f6cad12027486f7e57485569ed67d
Stefan Dierauf <sdierauf@cs.washington.edu>
2014-11-10 11:15:54 -0800

    xmllll + added smart solver stubs

---
+++ src/LinearSmartCensusSolver.java
@@ -0,0 +1,11 @@
+/**
+ * Stefan Dierauf Nov 2014
+ */
+public class LinearSmartCensusSolver implements CensusSolver {
+
+
+  @Override
+  public Pair<Integer, Float> singleInteraction(int west, int south, int east, int north) {
+    return null;
+  }
+}
---
+++ src/ParallelSmartCensusSolver.java
@@ -0,0 +1,10 @@
+/**
+ * Stefan Dierauf Nov 2014
+ */
+public class ParallelSmartCensusSolver implements CensusSolver {
+
+  @Override
+  public Pair<Integer, Float> singleInteraction(int west, int south, int east, int north) {
+    return null;
+  }
+}
