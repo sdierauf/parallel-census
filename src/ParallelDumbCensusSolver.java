c577fc41c325b405b09f437c8600aa82e12896c5
Stefan Dierauf <sdierauf@cs.washington.edu>
2014-11-12 20:32:27 -0800

    dem forkjoins

--- src/ParallelDumbCensusSolver.java
+++ src/ParallelDumbCensusSolver.java
@@ -1,12 +1,19 @@
+import java.util.concurrent.ForkJoinPool;
+
 /**
  * Stefan Dierauf Nov 2014
  * Parallel Dumb CensusSolver
  * aka v2
  */
+
+
 public class ParallelDumbCensusSolver implements CensusSolver {
+  protected static int CUTOFF = 500;
+  protected ForkJoinPool pool = new ForkJoinPool();
 
   private final int columns;
   private final int rows;
+
 
   public ParallelDumbCensusSolver(int columns, int rows, CensusData data) {
     this.columns = columns;
