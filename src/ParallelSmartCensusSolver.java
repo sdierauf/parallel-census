c3b6607f2336dd2c266b1df88b4cb4cb4bb42d52
Jack Conger <mail@jpconger.com>
2014-11-19 21:55:42 -0800

    Switched dumb & smart

--- src/ParallelSmartCensusSolver.java
+++ src/ParallelSmartCensusSolver.java
@@ -8,6 +8,7 @@ import java.util.concurrent.RecursiveTask;
 public class ParallelSmartCensusSolver extends SmartCensusSolver implements CensusSolver {
   protected ForkJoinPool pool = new ForkJoinPool();
   protected static int CUTOFF = 200;
+  public static int globalSum = 0;
 
   protected CensusData data;
 
@@ -45,7 +46,7 @@ public class ParallelSmartCensusSolver extends SmartCensusSolver implements Cens
     }
 
     protected int[][] compute() {
-      if (maxIndex - minIndex <= CUTOFF * 750) { // CUTOFF * 750 ?
+      if (maxIndex - minIndex <= CUTOFF * 10) {
         partialPop = 0;
         for (int i = minIndex; i < maxIndex; i++) {
           CensusGroup group = data.data[i];
@@ -71,9 +72,9 @@ public class ParallelSmartCensusSolver extends SmartCensusSolver implements Cens
         this.partialPop = fork.partialPop + main.partialPop;
 
         ParallelAddMatrices adder = new ParallelAddMatrices(fork.grid, main.grid, 0,
-            fork.grid.length, 0, fork.grid[0].length);
+            fork.grid.length, 0, fork.grid[0].length, this.grid);
         adder.compute();
-        return fork.grid;
+        return this.grid;
       }
     }
   }
@@ -82,39 +83,41 @@ public class ParallelSmartCensusSolver extends SmartCensusSolver implements Cens
   private class ParallelAddMatrices extends RecursiveAction {
     public int[][] data1;
     public int[][] data2;
+    public int[][] finalGrid;
     public int minXIndex;
     public int maxXIndex;
     public int maxYIndex;
     public int minYIndex;
 
     public ParallelAddMatrices(int[][] data1, int[][] data2, int minXIndex, int maxXIndex,
-                               int minYIndex, int maxYIndex) {
+                               int minYIndex, int maxYIndex, int[][] finalGrid) {
       this.data1 = data1;
       this.data2 = data2;
       this.minXIndex = minXIndex;
       this.maxXIndex = maxXIndex;
       this.minYIndex = minYIndex;
       this.maxYIndex = maxYIndex;
+      this.finalGrid = finalGrid;
     }
 
     protected void compute() {
-      if (Math.max(maxXIndex - minXIndex, maxYIndex - minYIndex) <= CUTOFF / 20) {
+      if (Math.max(maxXIndex - minXIndex, maxYIndex - minYIndex) <= CUTOFF) {
         for (int i = minXIndex; i < maxXIndex; i++) {
           for (int j = minYIndex; j < maxYIndex; j++) {
-            data1[i][j] = data1[i][j] + data2[i][j];
+            finalGrid[i][j] = data1[i][j] + data2[i][j];
           }
         }
       } else {
         int midwayX = (maxXIndex - minXIndex) / 2 + minXIndex;
         int midwayY = (maxYIndex - minYIndex) / 2 + minYIndex;
         ParallelAddMatrices fork = new ParallelAddMatrices(data1, data2, minXIndex,
-            midwayX, minYIndex, midwayY);
+            midwayX, minYIndex, midwayY, finalGrid);
         ParallelAddMatrices fork2 = new ParallelAddMatrices(data1, data2, minXIndex,
-            midwayX, midwayY, maxYIndex);
+            midwayX, midwayY, maxYIndex, finalGrid);
         ParallelAddMatrices fork3 = new ParallelAddMatrices(data1, data2, midwayX,
-            maxXIndex, minYIndex, midwayY);
+            maxXIndex, minYIndex, midwayY, finalGrid);
         ParallelAddMatrices main = new ParallelAddMatrices(data1, data2, midwayX,
-            maxXIndex, midwayY, maxYIndex);
+            maxXIndex, midwayY, maxYIndex, finalGrid);
         fork.fork();
         fork2.fork();
         fork3.fork();
